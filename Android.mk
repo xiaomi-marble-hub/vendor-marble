86ca90fcdc24ca5d33d9d3a435de5a2f168964d4
SharmagRit <sharmagritstuff@gmail.com>
2025-03-09 16:38:26 +0100

    marble: Stop including fw

--- Android.mk
+++ Android.mk
@@ -3,28 +3,3 @@
 #
 
 LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),marble)
-
-$(call add-radio-file-sha1-checked,radio/abl.img,65da17117a2afb327b80fb1d513cebc480af0c8b)
-$(call add-radio-file-sha1-checked,radio/aop.img,5449279045af8d42e21f5d60e95f79f9e33cce30)
-$(call add-radio-file-sha1-checked,radio/aop_config.img,65906d701843386d13c7fdc8b940e1e1a1bfd43d)
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,fe57d18477a7a2826d347fed21a1778d169e84bb)
-$(call add-radio-file-sha1-checked,radio/cpucp.img,3d009175156fb0b62524782a331e0724b5948024)
-$(call add-radio-file-sha1-checked,radio/devcfg.img,f9f0068d9faeb6cede873a4f4f0d1bf01e7ed78b)
-$(call add-radio-file-sha1-checked,radio/dsp.img,abb0cf432db0d513601b5314b55b1765dc900317)
-$(call add-radio-file-sha1-checked,radio/featenabler.img,eddf1291e0cb882e63074044d334fd1d0a00ba93)
-$(call add-radio-file-sha1-checked,radio/hyp.img,5fea542e5fb0c7ce2b2e7fbdb117df030433edc1)
-$(call add-radio-file-sha1-checked,radio/imagefv.img,e31ccd6b213e702a065e403103a1b811647cc0ba)
-$(call add-radio-file-sha1-checked,radio/keymaster.img,4e895c9d8bf8eedab5153eb7b7340b822490a612)
-$(call add-radio-file-sha1-checked,radio/modem.img,fcf36baf16208e82410f6f3f02d0baf7954dba48)
-$(call add-radio-file-sha1-checked,radio/qupfw.img,de851740130d9c504ac0ac133e33757bb8418956)
-$(call add-radio-file-sha1-checked,radio/shrm.img,432e6ad3c5e14f2082590cb5c41d2ec24f318a03)
-$(call add-radio-file-sha1-checked,radio/tz.img,0279f397d222ea77900ebf61d56ee7ab7d3f02d8)
-$(call add-radio-file-sha1-checked,radio/uefi.img,a7bdb6bc805f5264b7eb13c13879d78b27b9141d)
-$(call add-radio-file-sha1-checked,radio/uefisecapp.img,06002f8bf26f4a596f3e3f7453fa1998df9e2d16)
-$(call add-radio-file-sha1-checked,radio/xbl.img,4277060ab4bd0dac42e2081798b1b370a57b3177)
-$(call add-radio-file-sha1-checked,radio/xbl_config.img,2c7a5a57ea8532dca67e03f3976427b0d71c47dc)
-$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,b8e1096e905be39e2bffe38a780d92ae4c038095)
-
-endif
